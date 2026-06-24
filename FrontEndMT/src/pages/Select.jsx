373e94323ff61e6b91af6831d3597e2ab28f6300
Mateus <matheusmattosjf09@gmail.com>
2026-06-24 02:39:57 -0300

    atualizeggs

--- FrontEndMT/src/pages/Select.jsx
+++ FrontEndMT/src/pages/Select.jsx
@@ -1,42 +1,178 @@
 import './Select.css'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Select() {
-  return (
-    <>
-    <div className="FullBg">
 
+const navigate = useNavigate();
 
-        <div className="PlayerCard">
-        {/*Chamada de API para a primeira escolha de personagem (Do jogador), aqui é onde fica a foto, então tem que fazer uma "lib"
-        pra colocar uma foto de acordo com o nome ou ID do personagem selecionado*/}
-        <img src='imagens/personagens/Sasukeggs.png' alt="" />
-        </div>  
+const personagens = [
+{
+id:1,
+nome:"Naruto",
+icon:"imagens/personagens/icon/Narutoicon.png",
+foto:"imagens/personagens/Naruteggs.png"
+},
+{
+id:2,
+nome:"Sasuke",
+icon:"imagens/personagens/icon/SasukeIcon.png",
+foto:"imagens/personagens/Sasukeggs.png"
+},
+{
+id:3,
+nome:"Kakashi",
+icon:"imagens/personagens/icon/Kakashiicon.png",
+foto:"imagens/personagens/Kakasheggs.png"
+},
+{
+id:4,
+nome:"Pain",
+icon:"imagens/personagens/icon/Painicon.png",
+foto:"imagens/personagens/Painggs.png"
+},
+{
+id:5,
+nome:"Itachi",
+icon:"imagens/personagens/icon/Itachiicon.png",
+foto:"imagens/personagens/Itachieggs.png"
+},
+{
+id:6,
+nome:"Orochimaru",
+icon:"imagens/personagens/icon/Orochicon.png",
+foto:"imagens/personagens/Orocheggs.png"
+}
+];
 
-        <div className="versus">
-            <img src='imagens/background/Versus.png' alt="" />
-        </div>
+const [jogador,setJogador] =
+useState(personagens[0]);
 
-        <div className="personagens">
-            <div className="selecoes">
-                <div className="personagem"></div>
-                <div className="personagem"></div>
-                <div className="personagem"></div>
-                <div className="personagem"></div>
-                <div className="personagem"></div>
-                <div className="personagem"></div>
-                
-            </div>
-        </div>  
+const [inimigo,setInimigo] =
+useState(personagens[1]);
 
+const [ladoSelecionado,setLadoSelecionado] =
+useState("jogador");
 
-        <div className="EnemyCard">
-        {/*Chamada de API para a segunda escolha de personagem (Do inimigo), aqui é onde fica a foto, então tem que fazer uma "lib"
-        pra colocar uma foto de acordo com o nome ou ID do personagem selecionado*/}
-        <img src='imagens/personagens/Naruteggs.png' alt="" />
-        </div>
-    </div>
-    </>
-  );
+return (
+<>
+
+<div className="FullBg">
+
+<div
+className={`PlayerCard ${
+ladoSelecionado==="jogador"
+? "ladoAtivo"
+: ""
+}`}
+
+onClick={()=>
+setLadoSelecionado("jogador")
+}
+>
+
+<img
+src={jogador.foto}
+alt=""
+/>
+
+</div>
+
+<div className="versus">
+<img src='imagens/background/Versus.png' alt="" />
+</div>
+
+<div className="personagens">
+
+<div className="selecoes">
+
+{personagens.map((personagem)=>(
+
+<div
+key={personagem.id}
+
+className={`personagem ${
+(
+ladoSelecionado==="jogador"
+&& jogador.id===personagem.id
+)
+
+||
+
+(
+ladoSelecionado==="inimigo"
+&& inimigo.id===personagem.id
+)
+
+? "ativo"
+: ""
+
+}`}
+
+onClick={()=>{
+
+if(ladoSelecionado==="jogador"){
+setJogador(personagem);
+}
+
+else{
+setInimigo(personagem);
+}
+
+}}
+>
+
+<img
+src={personagem.icon}
+alt={personagem.nome}
+/>
+
+</div>
+
+))}
+
+</div>
+
+</div>
+
+<div
+className="botao"
+onClick={() =>
+navigate('/batalha')
+}
+>
+
+<img
+src="imagens/Confirmar.png"
+alt=""
+/>
+
+</div>
+
+<div
+className={`EnemyCard ${
+ladoSelecionado==="inimigo"
+? "ladoAtivo"
+: ""
+}`}
+
+onClick={()=>
+setLadoSelecionado("inimigo")
+}
+>
+
+<img
+src={inimigo.foto}
+alt=""
+/>
+
+</div>
+
+</div>
+
+</>
+);
+
 }
 
 export default Select;
